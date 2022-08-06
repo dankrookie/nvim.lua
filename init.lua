b4921ad822a99db9a8d1f1ac880ca591ff600037
dankrookie <mailsforonlybananas@gmail.com>
2022-08-07 00:57:27 +0100

    Minimal init.lua

---
+++ init.lua
@@ -0,0 +1,37 @@
+vim.opt.number = true
+vim.cmd([[packadd packer.nvim]])
+require("packer").startup(function()
+  use("wbthomason/packer.nvim")
+  use("morhetz/gruvbox")
+  vim.cmd([[colorscheme gruvbox]])
+  use("nvim-treesitter/nvim-treesitter")
+  require("nvim-treesitter.configs").setup({
+    ensure_installed = { "c", "lua", "rust" },
+    highlight = {
+      enable = false,
+    },
+  })
+  use("nvim-lualine/lualine.nvim")
+  require("lualine").setup({
+    options = {
+      icons_enabled = true,
+      theme = "ayu_dark",
+    },
+  })
+  use("junegunn/fzf")
+  use("junegunn/fzf.vim")
+end)
+vim.opt.list = true
+vim.opt.listchars:append("tab:> ")
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+vim.opt.swapfile = false
+vim.opt.wrap = false
+-- Map global leader from \ to Space
+vim.g.mapleader = " "
+-- Open recently used files
+vim.api.nvim_set_keymap("n", "<leader>fr", ":History<CR>", { noremap = true })
+-- Open files in same directory as current file
+vim.api.nvim_set_keymap("n", "<leader>ff", ":e %:h/<C-d>", { noremap = true })
