6e30474377b818d5dcaccb640e00edb8f6c2971f
dankrookie <mailsforonlybananas@gmail.com>
2023-02-04 12:31:22 +0000

    Add old vimrc for comparison

--- init.lua
+++ init.lua
@@ -20,6 +20,22 @@ require("packer").startup(function()
   })
   use("junegunn/fzf")
   use("junegunn/fzf.vim")
+  use{
+      "nvim-telescope/telescope.nvim", 
+      requires = { {'nvim-lua/plenary.nvim'} }}
+  require('telescope').setup {
+      pickers = {
+          find_files = {
+            hidden = true
+          },
+          live_grep = {
+            additional_args = function(opts)
+                return {"--hidden"}
+            end
+          }
+
+      }
+  }
 end)
 vim.opt.list = true
 vim.opt.listchars:append("tab:> ")
@@ -35,5 +51,16 @@ vim.g.mapleader = " "
 vim.api.nvim_set_keymap("n", "<leader>fr", ":History<CR>", { noremap = true })
 -- Open files in same directory as current file
 vim.api.nvim_set_keymap("n", "<leader>ff", ":e %:h/<C-d>", { noremap = true })
--- Persist undo tree across neovim sessions
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
 vim.opt.undofile = true
+
+
+-- openionated keymaps
+-- jj
+-- <lead>n
+-- <lead>s
+-- <lead>v
